2246c3506d8fed4255db0b908ed3f6522cd11c4d
Pravin <pravinavhad3597@gmail.com>
2025-04-07 15:28:15 +0530

    Sign Up Page Created

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+export default function SignUp(){
+    return (
+        <>
+            <div className="bg-white w-full h-screen">
+                <Input/>
+                <Button/>
+            </div>
+        </>
+    )
+}
